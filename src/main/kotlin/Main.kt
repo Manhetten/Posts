4d1050d517c28bc7548a73d4d80f23f3f2a8b9f9
Roman Avanesyan <creker88@rambler.ru>
2023-06-01 13:33:07 +0300

    HW6T1 commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -24,11 +24,66 @@ data class Post(
     val createdBy: Int = 123456,
     val date: Int = System.currentTimeMillis().toInt(),
     val text: String = "Hello World",
+    val replyOwnerId: Int = 123456,
+    val replyPostId: Int = 1234567890,
+    val friendsOnly: Int = 1,
+    val comments: Comments = Comments(),
+    val copyright: Copyright = Copyright(),
+    val reposts: Reposts = Reposts(),
+    val views: Views = Views(),
     val postType: String = "post",
+    val postSource: PostSource? = null,
+    val geo: Geo? = null,
+    val signerId: Int = 123456,
+    val copyHistory: Array<Reposts> = emptyArray(),
     val canPin: Boolean = true,
     val canDelete: Boolean = true,
     val canEdit: Boolean = true,
+    val isPinned: Int = 1,
+    val markedAsAds: Boolean = false,
+    val isFavorite: Boolean = false,
+    val donut: Donut? = Donut(),
     val likes: Likes = Likes()
+)
+
+class Donut(
+    val isDonut: Boolean = false,
+    val paidDuration: Int = 10,
+    val placeholder: Placeholder? = null,
+    val canPublishFreeCopy: Boolean = false,
+    val editMode: String = "all"
+)
+
+class Placeholder()
+class PostSource()
+class Copyright(
+    val id: Int = 1,
+    val link: String = "link",
+    val name: String = "name",
+    val type: String = "type"
+)
+class Geo(
+    val type: String = "place",
+    val coordinates: String = "12.05",
+    val place: Place = Place()
+)
+
+class Place(
+    val somePlace: Int = 0
+)
+class Views(
+    val count: Int = 0
+)
+class Reposts(
+    val count: Int = 0,
+    val userReposted: Boolean = false
+)
+class Comments(
+    val count: Int = 0,
+    val canPost: Boolean = true,
+    val groupsCanPost: Boolean = true,
+    val canClose: Boolean = false,
+    val canOpen: Boolean = false
 )
 
 class Likes(
